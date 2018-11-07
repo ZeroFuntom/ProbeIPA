93bbd827a3e224dd48787eff6bf35ea2fabfa1ff
ZeroFuntom <cassandracorrodi@yahoo.com>
2018-11-07 10:28:50 +0100

    Grundgerüst ASP.NET erstellt, Anfang Implementation der Datenbank, aktuelle Version der Doku

---
+++ Src/Secondhand.Domain/Model/SecondhandModel.edmx.sql
@@ -0,0 +1,92 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 11/07/2018 10:12:59
+-- Generated from EDMX file: F:\ProbeIPA\Src\Secondhand.Domain\Model\SecondhandModel.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [SecondhandDatabase];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'Users'
+CREATE TABLE [dbo].[Users] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [UserName] nvarchar(max)  NOT NULL,
+    [FirstName] nvarchar(max)  NOT NULL,
+    [LastName] nvarchar(max)  NOT NULL,
+    [Street] nvarchar(max)  NOT NULL,
+    [City] nvarchar(max)  NOT NULL,
+    [Zip] nvarchar(max)  NOT NULL,
+    [Country] nvarchar(max)  NOT NULL,
+    [FkItem] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'Items'
+CREATE TABLE [dbo].[Items] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [ItemName] nvarchar(max)  NOT NULL,
+    [Description] nvarchar(max)  NOT NULL,
+    [Year] nvarchar(max)  NOT NULL,
+    [Price] nvarchar(max)  NOT NULL,
+    [UserId] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Users'
+ALTER TABLE [dbo].[Users]
+ADD CONSTRAINT [PK_Users]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Items'
+ALTER TABLE [dbo].[Items]
+ADD CONSTRAINT [PK_Items]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [UserId] in table 'Items'
+ALTER TABLE [dbo].[Items]
+ADD CONSTRAINT [FK_UserItem]
+    FOREIGN KEY ([UserId])
+    REFERENCES [dbo].[Users]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_UserItem'
+CREATE INDEX [IX_FK_UserItem]
+ON [dbo].[Items]
+    ([UserId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
---
+++ Src/SecondhandTrade/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SecondhandTrade.MvcApplication" Language="C#" %>
