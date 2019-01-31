141dc1bf03b9c3fc04e68d9777efc6efc2dc8dd5
Cassandra Corrodi <cassandra.corrodi.external@atos.net>
2019-01-31 11:36:23 +0100

    controller for purchased implemented

--- Src/SecondhandTrade/Controllers/PurchasedController.cs
+++ Src/SecondhandTrade/Controllers/PurchasedController.cs
@@ -1,13 +1,29 @@
-﻿using System.Web.Mvc;
+﻿using System.Linq;
+using System.Web.Mvc;
+using Secondhand.BusinessLogic.Items;
+using Secondhand.BusinessLogic.Users;
+using Secondhand.Domain.Model;
+using SecondhandTrade.Models;
 
 namespace SecondhandTrade.Controllers
 {
     public class PurchasedController : Controller
     {
-        // GET: Purchased
+        private readonly IItemService _itemService;
+        private readonly IUserService _userService;
+
+        public PurchasedController(IItemService itemService, IUserService userService)
+        {
+            _itemService = itemService;
+            _userService = userService;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            string buyerName = System.Web.HttpContext.Current.User.Identity.Name;
+            int buyerId = _userService.GetUserByUserName(buyerName).Id;
+            var items = _itemService.GetPurchasedItems(buyerId);
+            return View(items);
         }
     }
 }
